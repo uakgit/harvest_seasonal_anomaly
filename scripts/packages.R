2575e785b99e5fcb82853ae6c984f882cc774d67
uakgit <uak2604@hotmail.com>
2024-03-23 13:03:19 +0100

    installs missing packages and loads

--- scripts/packages.R
+++ scripts/packages.R
@@ -1,6 +1,33 @@
 # Required packages ----
 
-library(date)
-library(ggplot2)
-library(foreign)
-library(dplyr)
+
+packages <- c(
+  "dplyr", "DT", "lubridate", "shinyjs", "data.table", "ggplot2",
+  "foreign", "date"
+)
+
+
+# # Install packages if not already installed
+suppressPackageStartupMessages(
+  invisible(
+    newPkgs <- packages[!(packages %in%
+                            installed.packages()[,"Package"])]))
+
+suppressPackageStartupMessages(
+  invisible(
+    if(
+      length(newPkgs) > 0
+    ){ install.packages(
+      newPkgs
+    )
+    }
+  )
+)
+
+
+# # Loads packages silently
+suppressPackageStartupMessages(
+  invisible(
+    lapply(packages, library, character.only = TRUE)
+  )
+)
